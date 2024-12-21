13defbebd3bcf9f716478c30b7b39b2f0208e872
Sarafmartinss <kaiodejesus098@gmail.com>
2024-12-21 00:54:00 -0300

    kaio

--- CRUD.php
+++ CRUD.php
@@ -3,19 +3,19 @@ abstract class CRUD {
     protected $pdo;
     protected $table;
 
-    // Construtor para definir o PDO e o nome da tabela
+    
     public function __construct($pdo, $table) {
         $this->pdo = $pdo;
         $this->table = $table;
     }
 
-    // Método para adicionar um registro
+    
     abstract public function add();
 
-    // Método para atualizar um registro
+   
     abstract public function update($field, $id, $value);
 
-    // Método para buscar um registro pelo ID
+   
     public function getById($id) {
         $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
         $stmt = $this->pdo->prepare($query);
@@ -24,7 +24,7 @@ abstract class CRUD {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    // Método para excluir um registro
+    
     public function delete($id) {
         $query = "DELETE FROM " . $this->table . " WHERE id = :id";
         $stmt = $this->pdo->prepare($query);
@@ -32,7 +32,7 @@ abstract class CRUD {
         return $stmt->execute();
     }
 
-    // Método para buscar todos os registros da tabela
+    
     public function getAll() {
         $query = "SELECT * FROM " . $this->table;
         $stmt = $this->pdo->prepare($query);
